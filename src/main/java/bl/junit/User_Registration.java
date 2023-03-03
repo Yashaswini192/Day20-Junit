1af8a524662e9efd87bbd01d652c60ee12ac965c
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-03-03 21:03:19 +0530

    Parametereized test to validate multiple entry for email address

--- src/main/java/bl/junit/User_Registration.java
+++ src/main/java/bl/junit/User_Registration.java
@@ -31,15 +31,15 @@ public class User_Registration {
 		boolean result1 = (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lastName));
 		if(!result1) {
 			return false;
-		//	System.out.println("invalid");
+			//	System.out.println("invalid");
 		}
 		return true;
 	}
 
-	public boolean checkEmail() {
+	public boolean checkEmail(String email) {
 		//UC3
 		System.out.println("Enter Email:");
-		String email = s.nextLine();
+		 email = s.nextLine();
 
 		boolean result2 = (Pattern.matches("^[A-z]+[a-zA-Z0-9]*[-._+/]?+[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$", email));
 		if(!result2) {
@@ -68,63 +68,73 @@ public class User_Registration {
 		String password = s.nextLine();
 
 		boolean result4 = (Pattern.matches("((?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8}$)", password));
-			if(!result4) {
-				//System.out.println("valid");
-				return false;
-			}
-			return true;
+		if(!result4) {
+			//System.out.println("valid");
+			return false;
 		}
+		return true;
+	}
 
+	public class MultipleEmailtest {
+		private String email1;
+	
 
-		public ArrayList<String> checkSampleEmail() {
-			System.out.println("Checking Sample Emails ");
-			// array for emails
-			ArrayList<String> email = new ArrayList<String>();
-			// valid EmailIDs
-			email.add("abc@yahoo.com");
-			email.add("abc-100@yahoo.com");
-			email.add("abc.100@yahoo.com");
-			email.add("abc111@abc.com");
-			email.add("abc-100@abc.net");
-			email.add("abc.100@abc.com.au");
-			email.add("abc@1.com");
-			email.add("abc@gmail.com.com");
-			email.add("abc+100@gmail.com");
-			// invalid EmailIDs
-			email.add("abc");
-			email.add("abc..");
-			email.add("abc..@gmail.com");
-			email.add("abc@abc@gmail.com");
-			email.add("abc@.com.my");
-			email.add("abc123@gmail.a");
-			email.add("abc123@.com");
-			email.add("abc123@.com.com");
-			email.add(".abc@abc.com");
-			email.add("abc()*@gmail.com");
-			email.add("abc..2002@gmail.com");
-			email.add("abc.@gmail.com");
-			email.add("abc@abc@gmail.com");
-			email.add("abc@gmail.com.1a");
-			email.add("abc..@gmail.com");
-			email.add("abc@gmail.com.aa.au");
-
-			String reGex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:([0-9]{1}|[a-zA-Z]{3,5})\\.)+[a-zA-Z]{2,3}";
-			// Creating/Defining a pattern to be searched
-			Pattern pattern = Pattern.compile(reGex);
-
-			//		for (String str : email) {
-			//			Matcher matcher = pattern.matcher(str);
-			//			if (matcher.matches()) {
-			//				System.out.println("valid email: " + str + " :" + matcher.matches());
-			//			} else {
-			//				System.out.println("Invalid email: " + str + " :" + matcher.matches());
-			//			}
-			//		}
-			return email;
-
-
+	public MultipleEmailtest(String email1) {
+			super();
+			this.email1 = email1;
 		}
+	}
+
+	public boolean checkSampleEmail(ArrayList<String> email) {
+		System.out.println("Checking Sample Emails ");
+		// array for emails
+		email = new ArrayList<String>();
+		// valid EmailIDs
+		email.add("abc@yahoo.com");
+		email.add("abc-100@yahoo.com");
+		email.add("abc.100@yahoo.com");
+		email.add("abc111@abc.com");
+		email.add("abc-100@abc.net");
+		email.add("abc.100@abc.com.au");
+		email.add("abc@1.com");
+		email.add("abc@gmail.com.com");
+		email.add("abc+100@gmail.com");
+		// invalid EmailIDs
+		email.add("abc");
+		email.add("abc..");
+		email.add("abc..@gmail.com");
+		email.add("abc@abc@gmail.com");
+		email.add("abc@.com.my");
+		email.add("abc123@gmail.a");
+		email.add("abc123@.com");
+		email.add("abc123@.com.com");
+		email.add(".abc@abc.com");
+		email.add("abc()*@gmail.com");
+		email.add("abc..2002@gmail.com");
+		email.add("abc.@gmail.com");
+		email.add("abc@abc@gmail.com");
+		email.add("abc@gmail.com.1a");
+		email.add("abc..@gmail.com");
+		email.add("abc@gmail.com.aa.au");
+
+		String reGex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:([0-9]{1}|[a-zA-Z]{3,5})\\.)+[a-zA-Z]{2,3}";
+		// Creating/Defining a pattern to be searched
+		Pattern pattern = Pattern.compile(reGex);
+
+				for (String str : email) {
+				Matcher matcher = pattern.matcher(str);
+					if (matcher.matches()) {
+						System.out.println("valid email: " + str + " :" + matcher.matches());
+					} else {
+						System.out.println("Invalid email: " + str + " :" + matcher.matches());
+					}
+				}
+		return true;
+
 
 	}
+
+
+}
 
 
--- src/test/java/com/bl/junit/test/User_Registration_Test.java
+++ src/test/java/com/bl/junit/test/User_Registration_Test.java
@@ -3,12 +3,16 @@ package com.bl.junit.test;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
 
 import bl.junit.User_Registration;
 
@@ -21,36 +25,35 @@ public class User_Registration_Test {
 
 		boolean firstname = user1.firstName();
 		Assertions.assertEquals(true, firstname);
-		Assertions.assertEquals(false, firstname);
+		//Assertions.assertEquals(false, firstname);
 	}
 
 	@Test
 	public void check_Second_Name() {
 		boolean lastname = user1.secondName();
 		Assertions.assertEquals(true, lastname);
-		Assertions.assertEquals(false, lastname);
+		//Assertions.assertEquals(false, lastname);
 	}
 
 	@Test
 	public void check_Email() {
-		boolean email = user1.checkEmail();
-		Assertions.assertEquals(true , email);
-		Assertions.assertEquals(false, email);
+		boolean email = user1.checkEmail("yashaswini@gmail.com");
+		Assertions.assertEquals(true , email,"yashaswini@gmail.com");
+		//Assertions.assertEquals(false, email);
 	}
 
 	@Test
 	public void check_Phone_Number() {
 		boolean phonenumber = user1.phoneNumber();
 		Assertions.assertEquals(true, phonenumber);
-		Assertions.assertEquals(false, phonenumber);
+		//Assertions.assertEquals(false, phonenumber);
 	}
 
 	@Test
 	public void check_Password() {
 		boolean password = user1.password();
 		Assertions.assertEquals(true ,password);
-		Assertions.assertEquals(false, password);
+		//Assertions.assertEquals(false, password);
 	}
-
 
 }
